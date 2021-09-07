c5c2d2c545b1ee6d4a63b8479e92ecf29f7b825d
Leonid <63495449+letimofeev@users.noreply.github.com>
2021-09-07 23:33:36 +0300

    added image loading

--- image/views.py
+++ image/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .forms import ImageForm
 
-# Create your views here.
+
+def index(request):
+    """Process images uploaded by users"""
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            # Get the current instance object to display in the template
+            img_obj = form.instance
+
+            return render(
+                request,
+                'index.html',
+                context={
+                    'form': form,
+                    'img_obj': img_obj
+                }
+            )
+    else:
+        form = ImageForm()
+
+    return render(
+        request,
+        'index.html',
+        context={'form': form})
+
--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'image.apps.ImageConfig',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
